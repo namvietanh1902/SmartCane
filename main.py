fe20a89a59ccd4144e107d44ed64f4bce1f3e288
namvietanh1902 <namvietanh2002@gmail.com>
2023-05-14 23:20:47 +0700

    return object name

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import cv2
 from yolo import YOLO_Process
 
-
+print(cv2.__version__)
 YOLO = YOLO_Process("best2.onnx","data2.yaml")
-img  = cv2.imread("102200165.jpg")
-#img = cv2.resize(img,(1000,600))
-output= YOLO.process_img(img)
+img  = cv2.imread("image/group2.jpg")
+
+object_name,output= YOLO.process_img(img)
+print(object_name)
+
 cv2.imshow("Result",output)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
 
